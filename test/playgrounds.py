07269d7db6f6b857a21d2a3ae8c8bac0887415d7
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2025-05-12 14:23:49 -0700

    Update playgrounds.py

--- test/playgrounds.py
+++ test/playgrounds.py
@@ -14,3 +14,20 @@ GPT4ALL offers various versions of gpt4all-j, vicuna, stable-vicuna, and wizardL
 GPT4ALL is top on my list as it provides an online demo, webUI, LangchainAPI, and desktop application for you to experience the state of the model on our laptop. It is simple and requires two steps to run the model on your machine. 
 
 GPT4ALL offers various versions of gpt4all-j, vicuna, stable-vicuna, and wizardLM. It also provides us with a CPU quantized GPT4All model checkpoint that can be run on any machine
+5. Chat LLM Hugging Face
+Chat LLM Hugging Face is a chatbot Gradio app hosted on Hugging Face Spaces. It lets you test Open assistant Pythia models, Google Flan, Big Science bloom and Bloomz, and EleutherAI GPT-NEOx. It is fast and simple and 
+requires no setup or signup. You can even access it through the embed link.
+
+6. Open Assistant
+Open Assistant is an open-source project led by the community, allowing users to test a range of cutting-edge models. The platform encourages contributions from anyone interested in improving the dataset and enhancing prompts by signing up for our service. 
+Presently, we offer different versions of LLaMA models; however, we plan to introduce advanced editions of StableLM and Pythia that can be utilized for commercial purposes.
+                                                                                                                                                   
+ 7. OPen Playground
+Open Playground lets you use all of your favorite LLMs models on your laptop using a Python package. The application either downloads the model from Hugging Face or lets you use the model directly using API. It offers models from OpenAI, Anthropic, Cohere, Forefront, HuggingFace, Aleph Alpha, and llama.cpp.
+
+Follow a guide from Cornellius Yudha Wijaya to step up APIs and use multiple models. 
+
+8. Hugging Chat
+HuggingChat is currently my favorite platform. It is fast, free, requires no signup, and provides the best-performing models for coding and general use. HuggingChat has a similar UI as ChatGPT, and you can use it for coding, math, research, and creative writing. 
+
+Recently, they have introduced the BigCode-StarCoder model for code generation in 86 programming languages. 
